5833f0bc05afbfa3c9a1e25a99758b3bc25da452
Khoi-Nguyen Mac <knmac@amazon.com>
2025-03-24 10:40:00 -0700

    Add language by mapping

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,17 +1,28 @@
 local ts_parser_root = os.getenv("HOME") .. "/.local/share/knvim/lazy/nvim-treesitter/parser/"
 
-if io.open(ts_parser_root) ~= nil then
-    vim.treesitter.language.add("python", { path = ts_parser_root .. "python.so" })
-    vim.treesitter.language.add("cpp", { path = ts_parser_root .. "cpp.so" })
+local language_map = {
+    python = { fname = "python.so", ext = { "py" } },
+    cpp = { fname = "cpp.so", ext = { "cpp" } }
+}
 
-    vim.treesitter.language.register("python", { "py" })
-    vim.treesitter.language.register("cpp", { "cpp" })
+if io.open(ts_parser_root) ~= nil then
+    for lang, value in pairs(language_map) do
+        vim.treesitter.language.add(lang, { path = ts_parser_root .. value["fname"] })
+        vim.treesitter.language.register(lang, value["ext"])
+    end
+
+    -- vim.treesitter.language.add("python", { path = ts_parser_root .. "python.so" })
+    -- vim.treesitter.language.add("cpp", { path = ts_parser_root .. "cpp.so" })
+    --
+    -- vim.treesitter.language.register("python", { "py" })
+    -- vim.treesitter.language.register("cpp", { "cpp" })
 
     -- Starts treesitter highlighting for a buffer
-    vim.api.nvim_create_autocmd( "FileType", { pattern = "py",
-    callback = function(args)
-        vim.treesitter.start(args.buf, "python")
-        vim.bo[args.buf].syntax = "on"  -- only if additional legacy syntax is needed
-    end
-})
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "py",
+        callback = function(args)
+            vim.treesitter.start(args.buf, "python")
+            vim.bo[args.buf].syntax = "on" -- only if additional legacy syntax is needed
+        end
+    })
 end
