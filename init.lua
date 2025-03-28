87a44c7ad9b3a0556ce4b0d8cb54b89b05288eef
Khoi-Nguyen Mac <knmac@amazon.com>
2025-03-28 14:11:48 -0700

    Use default colorscheme

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 require("core")
 
-local colorschemes = { "sorbet", "habamax", }
-for _, colorscheme in ipairs(colorschemes) do
-    if pcall(vim.cmd.colorscheme, colorscheme) then
-        break
-    end
-end
+-- local colorschemes = { "sorbet", "habamax", }
+-- for _, colorscheme in ipairs(colorschemes) do
+--     if pcall(vim.cmd.colorscheme, colorscheme) then
+--         break
+--     end
+-- end
